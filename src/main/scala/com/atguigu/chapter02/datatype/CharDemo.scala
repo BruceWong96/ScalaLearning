6133a34f514139db5e0541e249ebf3d8bd523205
FerdinandWang <603334301@qq.com>
2020-03-14 13:43:07 +0800

    修改git忽略文件

---
+++ src/main/scala/com/atguigu/chapter02/datatype/CharDemo.scala
@@ -0,0 +1,16 @@
+package com.atguigu.chapter02.datatype
+
+object CharDemo {
+  def main(args: Array[String]): Unit = {
+    //当输出一个char类型时，会输出该数字对应的字符（码值表unicode） //unicode包括ascii
+    var char1 : Char = 97
+    println("char1=" + char1)
+
+    //char可以当做数字进行运算
+    var char2 : Char = 'a'
+    var num = 10 + char2
+    println("num = " + num)
+
+  }
+
+}
